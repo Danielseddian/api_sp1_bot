f400a9f431a608a9d05cf437740cc691285e0b14
Кучин Денис <danielseddian@yandex.ru>
2021-05-04 14:40:31 +0300

    2021-05-04 14:40 Рабочий код без юнитестов

--- homework.py
+++ homework.py
@@ -1,62 +1,85 @@
 import logging
 import os
 import time
-from logging.handlers import RotatingFileHandler
+# from logging.handlers import RotatingFileHandler
 
 import requests
 import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s, %(levelname)s, %(message)s, %(processName)s',
-    filename='main.log',
-    filemode='a'
-)
 
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+HEADERS = {'Authorization': 'OAuth ' + PRAKTIKUM_TOKEN}
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+LOGGING_FORMAT = '%(asctime)s, %(levelname)s, %(message)s, %(processName)s'
+# PATH = os.path.expanduser('~') + __name__ + '.log
+RESPONSE_ERRORS = ['error', 'code']
+VERDICTS = {'rejected': 'К сожалению в работе нашлись ошибки.',
+            'reviewing': 'Работа взята на проверку',
+            'approved': 'Ревьюеру всё понравилось, можно приступать '
+                        'к следующему уроку.'}
+CHECKED = 'Проверена работа "{homework_name}"!\n\n{verdict}'
+UNKNOWN_STATUS = 'Неизвестный статус:\n\n{status}'
+BOT_ERROR = 'Бот столкнулся с ошибкой: {exception}'
+UNKNOWN_RESPONSE = 'Неожиданный ответ от сервера: {error}'
+
+
+def make_logfile_path():
+    root_path = os.path.expanduser('~')
+    os.chdir(root_path)
+    if not os.path.isdir('log_journal'):
+        os.mkdir('log_journal')
+    return root_path + '/log_journal/' + __name__ + '.log'
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = ('Ревьюеру всё понравилось, можно приступать '
-                   'к следующему уроку.')
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    try:
+        homework_name = homework['homework_name']
+        status = homework['status']
+        if status in VERDICTS:
+            verdict = VERDICTS[status]
+            return CHECKED.format(homework_name=homework_name,
+                                  verdict=verdict)
+        return UNKNOWN_STATUS.format(status=status)
+
+    except Exception as exception:
+        return BOT_ERROR.format(exception=exception)
 
 
-def get_homework_statuses(current_timestamp):
+def get_response(current_timestamp):
     data = {'from_date': current_timestamp}
-    headers = {'Authorization': 'OAuth ' + PRAKTIKUM_TOKEN}
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        params=data, headers=headers
-    )
-    return homework_statuses.json()
+    try:
+        response = requests.get(URL, params=data, headers=HEADERS).json()
+        for error in RESPONSE_ERRORS:
+            if error in response:
+                logging.error(UNKNOWN_RESPONSE.format(error=response[error]))
+        response[0]
+    except Exception as exception:
+        response = exception
+    return response
 
 
 def send_message(message, bot_client):
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    handler = RotatingFileHandler('main.log', maxBytes=(5 * pow(10, 6)),
-                                  backupCount=10)
-    logger.addHandler(handler)
     return bot_client.send_message(CHAT_ID, message)
 
 
 def main():
-    current_timestamp = int(time.time())
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format=LOGGING_FORMAT,
+        filename=make_logfile_path(),
+        filemode='a'
+    )
+    current_timestamp = int(time.time()) - 2678400
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
 
     while True:
         try:
-            new_homework = get_homework_statuses(current_timestamp)
+            new_homework = get_response(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(
                     new_homework.get('homeworks')[0]), bot_client
@@ -66,7 +89,13 @@ def main():
             time.sleep(300)
 
         except Exception as exception:
-            send_message(f'Бот столкнулся с ошибкой: {exception}', bot_client)
+            logger = logging.getLogger()
+            logger.error(exception)
+            try:
+                send_message(BOT_ERROR.format(exception=exception), bot_client)
+            except Exception as send_exception:
+                if str(send_exception) != str(exception):
+                    logger.error(send_exception)
             time.sleep(5)
 
 
